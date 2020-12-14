05da964043d56132ed4d92f5ee40a3ff7a1d2486
Srevin Saju <srevinsaju@sugarlabs.org>
2020-12-14 18:13:53 +0300

    ci: explicitly prepend command with qemu on arm32/aarch64

--- ci/test-appimage.sh
+++ ci/test-appimage.sh
@@ -33,14 +33,22 @@ if [[ "$PATCH_OUT_MAGIC_BYTES" != "" ]]; then
     dd if=/dev/zero of="$appimagetool" bs=1 count=3 seek=8 conv=notrunc
 fi
 
+NEEDS_QEMU=""
+if [[ "$ARCH" == "armhf" ]]; then
+    NEEDS_QEMU="qemu-arm"
+fi
+if [[ "$ARCH" == "aarch64" ]]; then
+    NEEDS_QEMU="qemu-aarch64"
+fi
+
 # first of all, try to run appimagetool
-"$appimagetool" && error  # should fail due to missing parameter
-"$appimagetool" -h || error  # should not fail
+$NEEDS_QEMU "$appimagetool" && error  # should fail due to missing parameter
+$NEEDS_QEMU "$appimagetool" -h || error  # should not fail
 
 # print version and update information
-"$appimagetool" --version || error  # should not fail
-"$appimagetool" --appimage-version || error  # should not fail
-"$appimagetool" --appimage-updateinformation || error  # should not fail
+$NEEDS_QEMU "$appimagetool" --version || error  # should not fail
+$NEEDS_QEMU "$appimagetool" --appimage-version || error  # should not fail
+$NEEDS_QEMU "$appimagetool" --appimage-updateinformation || error  # should not fail
 
 echo "" >&2
 echo "Tests successful!" >&2
